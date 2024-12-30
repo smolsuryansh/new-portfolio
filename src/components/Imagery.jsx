f36a2dc15bd0d5b3650a927b079f69c4bb6394c0
Suryansh Dubey <suryanshdubey2525@gmail.com>
2024-12-30 15:58:54 +0530

    minor imagery bug fix

--- src/components/Imagery.jsx
+++ src/components/Imagery.jsx
@@ -20,7 +20,7 @@ const Imagery = () => {
     return (
         <div>
             <div id='imagery' className='flex items-center justify-between w-full px-[5vw] py-[6vw] mt-[18vw] bg-[#102127]'>
-                <div id='imgLeft' className='w-[95%] md:w-[30%] h-auto'>
+                <div id='imgLeft' className='w-[95%] md:w-[30%] h-auto z-10'>
                     <h1 className='text-white leading-none md:text-[2vw]'>
                         I AM SURYANSH DUBEY, <br />
                         A PASSIONATE <br />
