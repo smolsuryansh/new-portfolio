7136bd88c962e47c27f67a24f1d1b76b561cb375
Suryansh Dubey <suryanshdubey2525@gmail.com>
2025-06-02 19:03:19 +0530

    fix loading of the beanco font

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+document.fonts.ready.then(() => {
+  document.body.style.visibility = 'visible';
+});
+document.body.style.visibility = 'hidden';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
